ec587173f59230349804c458f3068a6ae3f03d73
Tokoi Kazunori <trustsport12quruli@yahoo.co.jp>
2018-12-12 11:42:19 +0000

    users

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,28 +1,12 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
-
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
-
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
  
 }
-
-
-
-
-
-
-
-
-
-
-
-?>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,28 @@
 
 @section('content') 
 
-<div class="center jumbotron"> 
 @if(Auth::check())
-<h1>Welcome to the Sample App</h1>
 
 
-  <h2>
-    This is the home page for the
-    <a href="https://railstutorial.jp/">Ruby on Rails Tutorial</a>
-    sample application.
-  </h2>
-  
- <a href='/auth/login'><button class="btn btn-default">Log in</button></a>
-    
-  <!--<p><a href='http://rubyonrails.org'><img src="/mypage/resources/picture/btn027_05.gif" alt=""></a></p>-->
+<br>
+
+<div class="center jumbotron"> 
+こんにちは！
+
+<br>
+
+プロフィール：
+<br>
+メールアドレス→{{ \Auth::user()->email }}
+<br>
+名前→{{ \Auth::user()->name }}
+
+<br>
+<br>
+<br>
+<a href="/auth/logout">ログアウト</a>
+
+
 
   @else
   <h1>Welcome to the Sample App</h1>
@@ -27,7 +35,12 @@
     <a href="https://railstutorial.jp/">Ruby on Rails Tutorial</a>
     sample application.
   </h2>
-  <a href="/auth/register">Sign up!!</a>
+  <br>
+  <br>
+  <a href="/auth/register"><button class="btn btn-default">Sign up!!</button></a>
+  <a href='/auth/login'><button class="btn btn-default">Log in</button></a>
+    
+  
   @endif
   
 @endsection
